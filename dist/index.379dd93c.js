4a48eb40dba9e95b3b248244977619ee0f6a9845
Claire Annan <claire.annan@gsa.gov>
2021-12-15 19:19:12 -0500

    Add buildResponse pure function that filters and maps results by keyword. Retuns list of links.

--- dist/index.379dd93c.js
+++ dist/index.379dd93c.js
@@ -463,29 +463,33 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _axios = require("axios");
 var _axiosDefault = parcelHelpers.interopDefault(_axios);
 const form = document.querySelector('form');
-const textInput = document.getElementById('textInput');
-const container = document.getElementsByTagName('article')[0];
-const mascot = `<img src="https://emoji.slack-edge.com/T011QM4UN3G/a11y-ally/837f859d42c12db5.png"
-          alt="Ally the A11ybot Mascot"/>`;
-const randomFacts = 'https://raw.githubusercontent.com/18F/charlie-slack-bot-facts/main/a11y-facts.json';
 const resourcesJSON = 'https://raw.githubusercontent.com/18F/charlie/main/config/a11ybot-resources.json';
 // Add event listener to submit button. Show response when clicked.
 form.addEventListener('submit', (e)=>{
+    const textInput = document.getElementById('textInput');
     e.preventDefault();
     buildResponse(textInput.value);
 });
 const buildResponse = (keyword)=>{
     // get resources
     _axiosDefault.default.get(resourcesJSON).then((response)=>response.data
-    ).then((data)=>data.map((resource)=>{
+    ).then((data)=>data.filter((link)=>{
+            if (link.keywords.indexOf(keyword) > -1) return true;
+        }).map((resource)=>{
             return `<li><a href="${resource.url}" alt="Link to ${resource.name}">${resource.name}</a></li>`;
         }).join('')
     ).then((HTMLString)=>{
         const list = document.createElement('ul');
+        const foundText = document.createElement('em');
+        const container = document.getElementsByTagName('article')[0];
+        const mascot = `<img src="https://emoji.slack-edge.com/T011QM4UN3G/a11y-ally/837f859d42c12db5.png"
+          alt="Ally the A11ybot Mascot"/>`;
         list.innerHTML = HTMLString;
+        foundText.innerHTML = `Here's what I found about <strong>"${keyword}"</strong>`;
+        container.before(foundText);
         container.innerHTML = mascot;
         container.append(list);
-    }).catch((error)=>console.log(error)
+    }).catch((error)=>console.log('Error', error)
     );
 };
 
